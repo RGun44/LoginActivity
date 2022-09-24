f48f113f70f3fa45e8c65a9aa50495030c23542e
vinndz <jeremykev186@gmail.com>
2022-09-24 18:28:30 +0700

    layout

---
+++ app/src/main/java/com/example/loginactivity/fragment/Profile_Fragment.kt
@@ -0,0 +1,20 @@
+package com.example.loginactivity.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.loginactivity.R
+
+class Profile_Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_, container, false)
+    }
+
+}
