4b0dd4044f0aa87df093637d39a16c3553a71fcd
vinndz <jeremykev186@gmail.com>
2022-09-24 20:12:23 +0700

    act

---
+++ app/src/main/java/com/example/loginactivity/editProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.loginactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class editProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
