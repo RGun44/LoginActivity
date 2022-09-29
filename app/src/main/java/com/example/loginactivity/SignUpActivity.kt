28711854540f817fb1311ebac3deb2d09f96fa89
vinndz <jeremykev186@gmail.com>
2022-09-29 20:32:48 +0700

    no 1 done

--- app/src/main/java/com/example/loginactivity/SignUpActivity.kt
+++ app/src/main/java/com/example/loginactivity/SignUpActivity.kt
@@ -1,9 +1,18 @@
 package com.example.loginactivity
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.loginactivity.databinding.ActivitySignUpBinding
 import com.example.loginactivity.room.User
 import com.example.loginactivity.room.UserDB
@@ -18,6 +27,9 @@ class SignUpActivity : AppCompatActivity(){
     private lateinit var binding: ActivitySignUpBinding
     private var userId: Int = 0
 
+    private val CHANNEL_ID_1 = "channel_notification_01"
+    private val notificationId1 = 101
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -25,6 +37,8 @@ class SignUpActivity : AppCompatActivity(){
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        createNotificationChannel()
         title = "User Login"
 
         var btnClear = binding.btnClear
@@ -47,6 +61,7 @@ class SignUpActivity : AppCompatActivity(){
         }
 
         btnSignUp.setOnClickListener(View.OnClickListener {
+            sendNotification1()
             var checkSignUp = false
             val getUsername: String = username.text.toString()
             val getPassword: String = password.text.toString()
@@ -87,6 +102,22 @@ class SignUpActivity : AppCompatActivity(){
         })
     }
 
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+
+            val channel1 =  NotificationChannel(CHANNEL_ID_1, name, NotificationManager.IMPORTANCE_DEFAULT ).apply{
+                description =descriptionText
+            }
+
+
+            val notificationManager : NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel1)
+        }
+    }
+
     fun setupListener() {
         var username = binding.etsignup
         var email = binding.etemail
@@ -106,6 +137,39 @@ class SignUpActivity : AppCompatActivity(){
                 )
             )
             finish()
+        }
+    }
+
+    private fun sendNotification1(){
+        val intent: Intent = Intent(this, LoginActivity::class.java).apply{
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this,0,intent,0)
+
+        val broadcastIntent: Intent = Intent(this, NotificationReceiver::class.java)
+        broadcastIntent.putExtra("toastMessage","Selamat Datang " + binding.etsignup.text.toString())
+        val actionIntent = PendingIntent.getBroadcast(this,0,broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val picture = BitmapFactory.decodeResource(resources,R.drawable.logo)
+        val builder = NotificationCompat.Builder(this,CHANNEL_ID_1)
+            .setSmallIcon(R.drawable.logo)
+            .setContentText("Berhasil Register")
+//                Big Picture Style
+            .setLargeIcon(picture)
+            .setStyle(NotificationCompat.BigPictureStyle()
+                .bigLargeIcon(null)
+                .bigPicture(picture))
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .setColor(Color.BLACK)
+            .setAutoCancel(true)
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+            .addAction(R.mipmap.ic_launcher, "Pesan", actionIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)){
+            notify(notificationId1,builder.build())
         }
     }
 
