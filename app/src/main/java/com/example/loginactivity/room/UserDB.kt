2ad3b7a97baa6b8f2b78510df544731e5d18d499
RGun44* <ricogunawan444@gmail.com*>
2022-09-29 14:03:37 +0700

    Allow Main Thread Query

--- app/src/main/java/com/example/loginactivity/room/UserDB.kt
+++ app/src/main/java/com/example/loginactivity/room/UserDB.kt
@@ -27,6 +27,6 @@ abstract class UserDB: RoomDatabase() {
                 context.applicationContext,
                 UserDB::class.java,
                 "user.db"
-            ).build()
+            ).allowMainThreadQueries().build()
     }
 }
