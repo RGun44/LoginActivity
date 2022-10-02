7ffd838b390d6002fdff86376d4ca4c34f24f7f6
RGun44* <ricogunawan444@gmail.com*>
2022-10-02 16:08:38 +0700

    Bikin Kelas Notif Receiver

--- app/src/main/java/com/example/loginactivity/EditPaketActivity.kt
+++ app/src/main/java/com/example/loginactivity/EditPaketActivity.kt
@@ -1,8 +1,18 @@
 package com.example.loginactivity
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.loginactivity.databinding.ActivityEditPaketBinding
 import com.example.loginactivity.room.Constant
 import com.example.loginactivity.room.Paket
 import com.example.loginactivity.room.PaketDB
@@ -14,11 +24,18 @@ import kotlinx.coroutines.launch
 class EditPaketActivity : AppCompatActivity() {
     val db by lazy { PaketDB(this) }
     private var paketId: Int = 0
+    private var binding: ActivityEditPaketBinding? = null
+    private val CHANNEL_ID_1 = "channel_notification_01"
+    private val CHANNEL_ID_2 = "channel_notification_02"
+    private val notificationId1 = 101
+    private val notificationId2 = 102
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_paket)
+        binding = ActivityEditPaketBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
         setupView()
+        createNotificationChannel()
         setupListener()
     }
 
@@ -55,6 +72,8 @@ class EditPaketActivity : AppCompatActivity() {
                 )
                 finish()
             }
+            sendNotification1()
+            sendNotification2()
         }
 
         button_update.setOnClickListener {
@@ -87,5 +106,89 @@ class EditPaketActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return super.onSupportNavigateUp()
+    }
+
+    private fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Notification Title"
+            val descriptionText = "Notification Descriptions"
+
+            val channel1 = NotificationChannel(CHANNEL_ID_1, name, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = descriptionText
+            }
+            val channel2 = NotificationChannel(CHANNEL_ID_2, name, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel1)
+            notificationManager.createNotificationChannel(channel2)
+        }
+    }
+
+    private fun sendNotification1(){
+        val intent : Intent = Intent(this,EditPaketActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val broadcastIntent : Intent = Intent(this,NotificationReceiver::class.java)
+        broadcastIntent.putExtra("toastMessage", binding?.editTujuan?.text.toString())
+        val actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(this,CHANNEL_ID_1)
+            .setSmallIcon(R.drawable.ic_baseline_looks_one_24)
+            .setContentTitle(binding?.editAsal?.text.toString())
+            .setContentText(binding?.editTujuan?.text.toString())
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .setStyle(NotificationCompat.BigTextStyle()
+                .setBigContentTitle("Register Sukses")
+                .bigText("Pesanan anda berhasil diregistrasi, mohon lanjut ke bagian pembayarannya")
+                .setSummaryText("dari PaketIn")
+            )
+            .setColor(Color.BLUE)
+            .setAutoCancel(true)
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+            .addAction(R.mipmap.ic_launcher,"Toast",actionIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId1,builder.build())
+        }
+    }
+
+    private fun sendNotification2(){
+        val intent : Intent = Intent(this,EditPaketActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val broadcastIntent : Intent = Intent(this,NotificationReceiver::class.java)
+        broadcastIntent.putExtra("toastMessage", binding?.editTujuan?.text.toString())
+        val actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(this,CHANNEL_ID_2)
+            .setSmallIcon(R.drawable.ic_baseline_looks_two_24)
+            .setContentTitle(binding?.editAsal?.text.toString())
+            .setContentText(binding?.editTujuan?.text.toString())
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .setStyle(NotificationCompat.InboxStyle()
+                .setBigContentTitle("List Data yang diregister")
+                .addLine(binding?.editAsal?.text.toString())
+                .addLine(binding?.editTujuan?.text.toString())
+                .addLine(binding?.editBobot?.text.toString())
+                .addLine(binding?.editPilihan?.text.toString())
+            )
+            .setColor(Color.BLUE)
+            .setAutoCancel(true)
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+            .addAction(R.mipmap.ic_launcher,"Toast",actionIntent)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId2,builder.build())
+        }
     }
 }
