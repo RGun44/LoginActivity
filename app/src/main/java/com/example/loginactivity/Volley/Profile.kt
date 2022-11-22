13d7d8b8b35d7a4ff66bb41eba67009838dc7a2f
melatisukma1666 <melatisukma083@gmail.com>
2022-11-22 19:25:19 +0700

    Profile dan ProfileApi

---
+++ app/src/main/java/com/example/loginactivity/Volley/Profile.kt
@@ -0,0 +1,11 @@
+package com.example.loginactivity.Volley
+
+class Profile (
+
+    var id: String,
+    var username: String,
+    var password: String,
+    var email: String,
+    var phonenumber: String,
+    var birthdate: String,
+)
